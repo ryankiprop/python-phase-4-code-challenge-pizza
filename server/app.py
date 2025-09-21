85b29e3d0d9150329d4721064cffb4d99bee20e5
Ryan Langat Jnr <ryanlangatjnr@gmail.com>
2025-09-21 09:13:50 +0300

    complete code challenge

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request
 from flask_restful import Api, Resource
 import os
 
@@ -14,16 +13,102 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
-
 api = Api(app)
-
 
 @app.route("/")
 def index():
-    return "<h1>Code challenge</h1>"
+    return "<h1>Code Challenge Pizza</h1>"
 
+class RestaurantsResource(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [restaurant.to_dict(only=('address', 'id', 'name')) for restaurant in restaurants], 200
+
+api.add_resource(RestaurantsResource, '/restaurants')
+
+class RestaurantResource(Resource):
+    def get(self, id):
+        restaurant = db.session.get(Restaurant, id)
+        if restaurant:
+            restaurant_data = restaurant.to_dict()
+            restaurant_data["restaurant_pizzas"] = []
+            
+            for rp in restaurant.restaurant_pizzas:
+                restaurant_data["restaurant_pizzas"].append({
+                    "id": rp.id,
+                    "pizza_id": rp.pizza_id,
+                    "restaurant_id": rp.restaurant_id,
+                    "price": rp.price,
+                    "pizza": {
+                        "id": rp.pizza.id,
+                        "name": rp.pizza.name,
+                        "ingredients": rp.pizza.ingredients
+                    }
+                })
+            
+            return restaurant_data, 200
+        return {"error": "Restaurant not found"}, 404
+
+    def delete(self, id):
+        restaurant = db.session.get(Restaurant, id)
+        if restaurant:
+            RestaurantPizza.query.filter_by(restaurant_id=id).delete()
+            db.session.delete(restaurant)
+            db.session.commit()
+            return '', 204
+        return {"error": "Restaurant not found"}, 404
+
+api.add_resource(RestaurantResource, '/restaurants/<int:id>')
+
+class PizzasResource(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [pizza.to_dict(only=('id', 'ingredients', 'name')) for pizza in pizzas], 200
+
+api.add_resource(PizzasResource, '/pizzas')
+
+class RestaurantPizzasResource(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_restaurant_pizza = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            
+            db.session.refresh(new_restaurant_pizza)
+            
+            response_data = {
+                "id": new_restaurant_pizza.id,
+                "price": new_restaurant_pizza.price,
+                "pizza_id": new_restaurant_pizza.pizza_id,
+                "restaurant_id": new_restaurant_pizza.restaurant_id,
+                "pizza": {
+                    "id": new_restaurant_pizza.pizza.id,
+                    "name": new_restaurant_pizza.pizza.name,
+                    "ingredients": new_restaurant_pizza.pizza.ingredients
+                },
+                "restaurant": {
+                    "id": new_restaurant_pizza.restaurant.id,
+                    "name": new_restaurant_pizza.restaurant.name,
+                    "address": new_restaurant_pizza.restaurant.address
+                }
+            }
+            
+            return response_data, 201
+            
+        except ValueError:
+            db.session.rollback()
+            return {"errors": ["validation errors"]}, 400
+        except Exception as e:
+            db.session.rollback()
+            return {"errors": ["validation errors"]}, 400
+
+api.add_resource(RestaurantPizzasResource, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/seed.py
+++ server/seed.py
@@ -4,38 +4,31 @@ from app import app
 from models import db, Restaurant, Pizza, RestaurantPizza
 
 with app.app_context():
-
-    # This will delete any existing rows
-    # so you can run the seed file multiple times without having duplicate entries in your database
-    print("Deleting data...")
-    Pizza.query.delete()
-    Restaurant.query.delete()
-    RestaurantPizza.query.delete()
+    print("Dropping tables...")
+    db.drop_all()
+    print("Creating tables...")
+    db.create_all()
 
     print("Creating restaurants...")
-    shack = Restaurant(name="Karen's Pizza Shack", address='address1')
-    bistro = Restaurant(name="Sanjay's Pizza", address='address2')
-    palace = Restaurant(name="Kiki's Pizza", address='address3')
-    restaurants = [shack, bistro, palace]
+    shack = Restaurant(name="Karen's Pizza Shack", address="address1")
+    bistro = Restaurant(name="Sanjay's Pizza", address="address2")
+    palace = Restaurant(name="Kiki's Pizza", address="address3")
 
     print("Creating pizzas...")
-
     cheese = Pizza(name="Emma", ingredients="Dough, Tomato Sauce, Cheese")
     pepperoni = Pizza(
-        name="Geri", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
+        name="Geri", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni"
+    )
     california = Pizza(
-        name="Melanie", ingredients="Dough, Sauce, Ricotta, Red peppers, Mustard")
-    pizzas = [cheese, pepperoni, california]
+        name="Melanie", ingredients="Dough, Sauce, Ricotta, Red peppers, Mustard"
+    )
 
     print("Creating RestaurantPizza...")
-
     pr1 = RestaurantPizza(restaurant=shack, pizza=cheese, price=1)
     pr2 = RestaurantPizza(restaurant=bistro, pizza=pepperoni, price=4)
     pr3 = RestaurantPizza(restaurant=palace, pizza=california, price=5)
-    restaurantPizzas = [pr1, pr2, pr3]
-    db.session.add_all(restaurants)
-    db.session.add_all(pizzas)
-    db.session.add_all(restaurantPizzas)
+
+    db.session.add_all([shack, bistro, palace, cheese, pepperoni, california, pr1, pr2, pr3])
     db.session.commit()
 
-    print("Seeding done!")
+    print("Seeding done! 🌱")
